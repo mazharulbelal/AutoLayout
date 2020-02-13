3be116f2103212e1a28e137d61172d52bb8f5882
Mazharul Belal <soft.belal@gmail.com>
2020-02-13 20:26:51 +0600

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Mazharul Belal on 13/2/20.
+//  Copyright © 2020 Mazharul Belal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
